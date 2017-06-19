40780ea16d1a669943e40d1397ba876567d4bf27
Wong Jianming <1249331134@qq.com>
2017-06-19 22:31:12 +0800

    API 接口实现了部分，未做授权验证

---
+++ code/qingjia_MVC/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace qingjia_MVC
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("datatype", "json", "application/json"));
+        }
+    }
+}
--- code/qingjia_MVC/Global.asax.cs
+++ code/qingjia_MVC/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using FineUIMvc;
 using Newtonsoft.Json.Linq;
+using System.Web.Http;
 
 namespace qingjia_MVC
 {
@@ -15,8 +16,9 @@ namespace qingjia_MVC
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalConfiguration.Configure(WebApiConfig.Register);//API路由注册必须在前面！！！！！！！我也不知道为啥，估计设定就是这样
 
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
             ModelBinders.Binders.Add(typeof(JArray), new JArrayModelBinder());
             ModelBinders.Binders.Add(typeof(JObject), new JObjectModelBinder());
         }
