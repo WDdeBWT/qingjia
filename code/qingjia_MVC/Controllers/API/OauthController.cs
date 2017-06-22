ebf1de6ba09d08bd820d18723ddbba12c0cc17f0
Wong Jianming <1249331134@qq.com>
2017-06-22 20:39:01 +0800

    0622 API接口-授权接口未完成

---
+++ code/qingjia_MVC/Controllers/API/OauthController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace qingjia_MVC.Controllers
+{
+    [RoutePrefix("api/oauth")]
+    public class OauthController : ApiController
+    {
+        [HttpGet]
+        public bool Authorize()
+        {
+            return true;
+        }
+
+        [HttpGet, Route("GetValue")]
+        public int value()
+        {
+            return 2;
+        }
+
+        [HttpGet, Route("GetValue")]
+        public int value(string code)
+        {
+            if (code != null && code == "2")
+            {
+                return 1;
+            }
+            return 2;
+        }
+    }
+}
