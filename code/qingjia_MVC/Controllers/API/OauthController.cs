e74e1c28b4e3a4a3595c2e42900f27664dc33db2
JianMingWang <1249331134@qq.com>
2017-06-24 17:20:45 +0800

    0624 授权接口已完成 添加API测试页

--- code/qingjia_MVC/Controllers/API/OauthController.cs
+++ code/qingjia_MVC/Controllers/API/OauthController.cs
@@ -9,6 +9,8 @@ using qingjia_MVC.Common;
 
 namespace qingjia_MVC.Controllers
 {
+    #region 数据模型
+
     public class User_Login
     {
         public string UserID { get; set; }
@@ -16,10 +18,12 @@ namespace qingjia_MVC.Controllers
         public string YiBanID { get; set; }
     }
 
-    public class ApiError
+    public class AuthorizeModel
     {
-
+        public string access_token { get; set; }
     }
+
+    #endregion
 
     [RoutePrefix("api/oauth")]
     public class OauthController : ApiController
@@ -28,21 +32,57 @@ namespace qingjia_MVC.Controllers
         private imaw_qingjiaEntities db = new imaw_qingjiaEntities();
 
         /// <summary>
-        /// 获取授权 Post请求 用户ID 用户Psd 易班用户ID
+        /// POST
+        /// 
+        /// 获取授权 用户ID 用户Psd 易班用户ID  返回给用户一个GUID当做AccessToken
         /// </summary>
         /// <param name="user_login_info"></param>
         /// <returns></returns>
-        [HttpPost]
-        public bool Authorize([FromBody]User_Login user_login_info)
+        [HttpPost, Route("authorize")]
+        public ApiBaseResult Authorize([FromBody]User_Login user_login_info)
         {
-            string UserID = user_login_info.UserID;
-            string UserPSd = user_login_info.UserPsd;
-            string YiBanID = user_login_info.YiBanID;
+            ApiBaseResult result = new ApiBaseResult();
+
+            string UserID = "";
+            string UserPSd = "";
+            string YiBanID = "";
+
+            #region 检查参数是否正确
+
+            if (user_login_info == null)
+            {
+                //参数错误
+                result.result = "error";
+                result.messages = "未接收到合法参数！";
+                return result;
+            }
+            else
+            {
+                //存在合法参数正确
+                try
+                {
+                    UserID = user_login_info.UserID;
+                    UserPSd = user_login_info.UserPsd;
+                    YiBanID = user_login_info.YiBanID;
+                }
+                catch
+                {
+                    result.result = "error";
+                    result.messages = "参数格式错误或缺少参数！";
+                    return result;
+                }
+            }
+
+            #endregion
+
+            #region 账号绑定
 
             var accountList = from T_Account in db.T_Account where (T_Account.YiBanID == YiBanID) select T_Account;
             if (accountList.Any())
             {
                 //此账号已绑定
+                result.result = "error";
+                result.messages = "此账号已绑定！";
             }
             else
             {
@@ -58,19 +98,71 @@ namespace qingjia_MVC.Controllers
                         account.YiBanID = YiBanID;
                         account.YB_AccessToken = GuidString;
                         db.SaveChanges();
+
+                        AuthorizeModel authorizeModel = new AuthorizeModel();
+                        authorizeModel.access_token = access_token;
+                        result.result = "success";
+                        result.data = authorizeModel;
                     }
                     else
                     {
                         //用户密码错误
+                        result.result = "error";
+                        result.messages = "账号密码错误！";
                     }
                 }
                 else
                 {
                     //此用户ID不存在
+                    result.result = "error";
+                    result.messages = "此用户ID不存在！";
                 }
             }
-            return true;
+
+            #endregion
+
+            return result;
         }
+
+        /// <summary>
+        /// GET
+        /// 
+        /// 获取AccessToken
+        /// </summary>
+        /// <param name="YiBanID"></param>
+        /// <returns></returns>
+        [HttpGet, Route("access_token")]
+        public ApiBaseResult Access_Token(string YiBanID)
+        {
+            ApiBaseResult result = new ApiBaseResult();
+
+            var userList = from T_Account in db.T_Account where (T_Account.YiBanID == YiBanID) select T_Account;
+            if (userList.Any())
+            {
+                //验证通过
+                string GuidString = Guid.NewGuid().ToString();
+                string access_token = "";
+                string UserID = userList.ToList().First().ID;
+                T_Account account = db.T_Account.Find(UserID);
+                access_token = account.ID + "_" + GuidString;
+                account.YiBanID = YiBanID;
+                account.YB_AccessToken = GuidString;
+                db.SaveChanges();
+
+                AuthorizeModel authorizeModel = new AuthorizeModel();
+                authorizeModel.access_token = access_token;
+                result.result = "success";
+                result.data = authorizeModel;
+            }
+            else
+            {
+                //尚未绑定YiBanID
+                result.result = "error";
+                result.messages = "尚未绑定账号的易班ID";
+            }
+            return result;
+        }
+
 
         [HttpGet, Route("GetValue")]
         public int value()
@@ -79,13 +171,23 @@ namespace qingjia_MVC.Controllers
         }
 
         [HttpGet, Route("GetValue")]
-        public int value(string code)
+        public ApiBaseResult value(string code)
         {
+            ApiBaseResult result = new ApiBaseResult();
+            result.result = "error";
+
+            User_Login loginInfo = new User_Login();
+            loginInfo.UserID = "0121403490106";
+            loginInfo.UserPsd = "123456";
+            loginInfo.YiBanID = "123456";
+
+            result.data = loginInfo;
+
             if (code != null && code == "2")
             {
-                return 1;
+                result.result = "succcess";
             }
-            return 2;
+            return result;
         }
     }
 }
---
+++ code/qingjia_MVC/Controllers/BaseApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qingjia_MVC.Controllers
+{
+    public class BaseApiController : Controller
+    {
+        // GET: BaseApi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
