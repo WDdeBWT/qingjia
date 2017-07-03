ed4a10382f5b765cd11da9994fc6539a7f78d533
JianMingWang <1249331134@qq.com>
2017-07-03 11:12:05 +0800

    0703  修复bug  已上传至服务器

--- code/qingjia_MVC/App_Start/RouteConfig.cs
+++ code/qingjia_MVC/App_Start/RouteConfig.cs
@@ -16,8 +16,8 @@ namespace qingjia_MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                //defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }//网站主页   正常运行网站时  将此设定为主页
-                defaults: new { controller = "BaseApi", action = "Index", id = UrlParameter.Optional }//API接口主页   测试API接口是  将此设定为主页
+                defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }//网站主页   正常运行网站时  将此设定为主页
+                //defaults: new { controller = "BaseApi", action = "Index", id = UrlParameter.Optional }//API接口主页   测试API接口是  将此设定为主页
             );
         }
     }
